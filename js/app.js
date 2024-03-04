acf79ffae1a36a6dfbd4355c3a66e2a6d6aff171
RoccoCerro <rcerro02@gmail.com>
2024-03-04 14:50:24 +0100

    ADD variabili e istruzioni if

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// oppure applicato uno sconto del 40% per gli over 65.
+// L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, 
+// per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.
+
+// console.log("ciao")
+
+// Chiedo al cliente il numero dei km che dovrà percorrere e i suoi anni.
+// SE il cliente ha meno di 18 anni moltiplico i km inseriti dal cliente per 0,21€ e sottraggo il 20%
+// SE il cliente ha più di 65 anni moltiplico i km inseriti dal cliente per 0,21€ e sottraggo il 40%
+// ALTRIMENTI moltiplico i km per inseriti dal cliente per 0,21€
+
+let km = 2;
+let anni = 68;
+let prezzo;
+
+if(anni<18){
+    let valore = (km * 0.21);
+    let sconto = (valore/100) * 20;
+    prezzo = valore - sconto;
+    console.log(prezzo);
+}
+else if(anni>65){
+    let valore = (km * 0.21);
+    let sconto = (valore/100) * 40;
+    prezzo = valore - sconto;
+    console.log(prezzo);
+}
+else{
+    let valore = (km * 0.21);
+    prezzo = valore
+    console.log(prezzo);
+}
